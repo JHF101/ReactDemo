4f27ab664ef5d028c68ef98e852d11d957254c1d
JHF101 <jh.faul4@gmail.com>
2023-01-13 15:52:41 +0200

    Add basic route protection

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Debugger from './components/debug';
 import Categories from './components/categories';
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
 
 // Environment is not working so doing hardcoded config
 
 // Run command to get the API token
 function App() {
-
+  const token = localStorage.getItem('token')
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
   return (
     <div className="App">
       {/* <Debugger/> */}
